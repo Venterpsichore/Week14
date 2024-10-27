a6f913693eedd3250e6aa289f6b7712bf065b3d7
Jase <176360272+Venterpsichore@users.noreply.github.com>
2024-10-27 10:43:09 -0700

    Adding last 3 files w/ comments

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import projects.exception.DbException;
 
 
-/*Test*/
-public class DbConnection {
 
+public class DbConnection {
+// Creating a uri made of host, port, schema, user, & password.
 	private static String HOST = "localhost";
 	private static String PASSWORD = "Vegiepan69!";
 	private static int PORT = 3306;
@@ -17,14 +17,18 @@ public class DbConnection {
 	
 	public static Connection getConnection() {
 		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s", HOST, PORT, SCHEMA, USER, PASSWORD);
-		try {
+		try { // Designating code to be checked for an error
+			
+			// Creating a connection connection to be returned
 			Connection connection = DriverManager.getConnection(uri);
 			System.out.println("Connection achieved.");
 			return connection;
+			
+			// Creating a catch for exception e and printing out a confirmation of connection failure
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Connection unsuccessful.");
-			throw new DbException("Connection unsuccessful.");
+			throw new DbException("Connection unsuccessful."); // Allows for a call 
 		}
 	}
 }
--- src/main/java/projects/exception/DbException.java
+++ src/main/java/projects/exception/DbException.java
@@ -1,16 +1,16 @@
 package projects.exception;
-
+// Extends unchecked RuntimeException 
 public class DbException extends RuntimeException{
 
-
+// Writes error message
 public DbException(String message) {
 	super(message);
 }
-
+// Takes thrown cause
 public DbException(Throwable cause) {
 	super(cause);
 }
-
+// Combines the message and cause
 public DbException(String message, Throwable cause) {
 	super(message, cause);
 }
