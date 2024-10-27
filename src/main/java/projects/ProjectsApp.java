8b623dcb6a1ba8411566c4271e6aa13d45d4b4de
Jase <176360272+Venterpsichore@users.noreply.github.com>
2024-10-26 21:08:31 -0700

    Comments added

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,7 +1,7 @@
 package projects;
 
 import projects.dao.DbConnection;
-
+//Driver Manager connecting the code to the database URL
 public class ProjectsApp {
 	 public static void main(String[] args) {
 	 DbConnection.getConnection();
