b0bb83e28b7f19f1f5a4ef853e292fda3835795b
Jase <176360272+Venterpsichore@users.noreply.github.com>
2024-12-15 18:54:26 -0700

    Updating PetStoreApplication w/ comments as to future work

--- eclipse-workspace/pet-store/src/main/java/pet/store/PetStoreApplication.java
+++ eclipse-workspace/pet-store/src/main/java/pet/store/PetStoreApplication.java
@@ -9,4 +9,14 @@ public class PetStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PetStoreApplication.class, args); // Bootstraps the app
 	}
+	
+/* Pending DBeaver integration & connection after attempts to each preceding web & AI search of given errors.
+ * 
+ * Schema was made and Allow Public Key Retrieval was toggled on after error.
+ * 
+ * spring-boot-starter dependency was added to no resolution.
+ * 
+ * Log in checked to see if connection matched and dialect checked to see if matched Run error advice. */
+	
+	
 }
